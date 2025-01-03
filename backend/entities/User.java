311be7111e9c7f6b19793463c90b254947ca302e
Anoushka Vyas <anuvyas2405@gmail.com>
2025-01-03 14:02:37 +0530

    Added User entity

---
+++ backend/entities/User.java
@@ -0,0 +1,38 @@
+package com.prepminds.backend.entities;
+
+import jakarta.persistence.*;
+
+@Entity//marks the class as a table in the database
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    //getters and setters:
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+}
